5b16f55f7409f661ab3dcec3a1f390e06deb079d
NGUYENVANVU <xitrum.xinh@gmail.com>
2022-06-06 21:17:29 +0700

    update axios

--- src/Api/request.js
+++ src/Api/request.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const ins = axios.create({
-    // baseURL: process.env.REACT_APP_BASE_URL,
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.REACT_APP_BASE_URL,
+    // baseURL: 'http://localhost:8000',
 });
 
 ins.interceptors.request.use(function(config) {
